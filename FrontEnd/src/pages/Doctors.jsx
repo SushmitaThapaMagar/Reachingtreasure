c521131a7717c19d4c4605a88c84fd3793cb52c0
SushmitaThapaMagar <sushmitachan44@gmail.com>
2025-04-21 19:08:58 +0545

    add

--- FrontEnd/src/pages/Doctors.jsx
+++ FrontEnd/src/pages/Doctors.jsx
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
 const {speciality} = useParams()
+
+const {filterDoc,setFilterDoc} = useState([])
 const {doctors} = useContext(AppContext)
 
   return (
@@ -17,6 +19,11 @@ const {doctors} = useContext(AppContext)
           <p>Pediatricians</p>
           <p>Neurologist</p>
           <p>Gastroenterologist</p>
+        </div>
+        <div>
+          {
+            filterDoc.map()
+          }
         </div>
       </div>
     </div>
