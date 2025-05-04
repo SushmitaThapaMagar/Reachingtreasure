7b4d845ff07dc3010a0b90678f2ba27fa3eef3a8
SushmitaThapaMagar <sushmitachan44@gmail.com>
2025-05-04 21:32:04 +0545

    add

--- BackEnd/controllers/doctorController.js
+++ BackEnd/controllers/doctorController.js
@@ -8,12 +8,24 @@ const changeAvailability = async (req, res) => {
         const docData = await doctorModel.findById(docId)
         
         await doctorModel.findByIdAndUpdate(docId,{available:!docData.available})
+        
         res.json({success:true, message: 'Availability Changed'})
 
     }catch(error){
         console.log(error);
         res.json({success:false, message:error.message})
-        
+    }
+}
+
+const doctorList = async(req, res) =>{
+    try{
+        const doctors = await doctorModel.find({}).select(['-password','-email'])
+
+        res.json({success:true, doctors})
+   
+    } catch(error){
+        console.log(error);
+        res.json({success:false, message:error.message})
 
     }
 }
--- BackEnd/routes/adminRoute.js
+++ BackEnd/routes/adminRoute.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { addDoctor, allDoctors, loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin from '../middlewares/authAdmin.js'
-import { changeAvailability } from './doctorRoute.js'
+import { changeAvailability } from '../controllers/doctorController.js'
 
 const adminRouter = express.Router()
 
